9c5ab7dfa58f39c316b47fb86b5cb743162443f7
Kresna <satyakresna@unud.ac.id>
2022-01-29 21:44:38 +0800

    load sidebar app from app_menu helper

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,18 +1,8 @@
 <nav id="sidebar" class="sidebar js-sidebar">
     <div class="sidebar-content js-simplebar">
-        <a class="sidebar-brand" href="{{ url('adminkit/pages/dashboard') }}">
+        <a class="sidebar-brand" href="{{ url('/') }}">
             <span class="align-middle">AdminKit</span>
         </a>
-        <ul class="sidebar-nav">
-            <li class="sidebar-header">
-                Pages
-            </li>
-
-            <li class="sidebar-item {{ is_active_path('/') ? 'active' : '' }}">
-                <a class="sidebar-link" href="{{ url('/') }}">
-                    <i class="align-middle" data-feather="book"></i> <span class="align-middle">Blank</span>
-                </a>
-            </li>
-        </ul>
+        {!! load_app_menu() !!}
     </div>
 </nav>
