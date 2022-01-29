6368ab0ab3210f06a37066780d2d5334f0e7f256
Kresna <satyakresna@unud.ac.id>
2022-01-29 21:44:12 +0800

    create home page

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+<h1 class="h3 mb-3">Home Page</h1>
+<div class="row">
+	<div class="col-12">
+		<div class="card">
+			<div class="card-header">
+				<h5 class="card-title mb-0">Make something even better!</h5>
+			</div>
+			<div class="card-body">
+                <p>This is your home. Welcome!</p>
+			</div>
+		</div>
+	</div>
+</div>
+@endsection
